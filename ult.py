71215198c76288ad5efde7236b52d1170caa825c
lipovka <lipovkaai@yandex.ru>
2023-04-26 16:53:13 +0700

    initial commit

---
+++ ult.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib
+import matplotlib.patches as patches
+import numpy as np
+from sklearn import  linear_model
+from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
+#from adjustText import adjust_text
